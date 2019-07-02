5c37ae210c46b610299b0fe67362ef2e8089d905
camila_fiaminghi <camila.fiaminghi@gmail.com>
2019-07-02 14:31:26 -0300

    improving device function

--- src/utils/device.js
+++ src/utils/device.js
@@ -1,6 +1,5 @@
-const device = {
-	type: (window.innerWidth > 544) ? (window.innerWidth <= 992 ? 'tablet' : 'desktop') : 'mobile',
-	isMobile: (window.innerWidth <= 544)
-}
+export const device = () => (window.innerWidth > 544) ? (window.innerWidth <= 992 ? 'tablet' : 'desktop') : 'mobile'
 
-export default device
+export const checkIsMobile = () => (window.innerWidth <= 544)
+
+export const compareDevice = (desktop, tablet, mobile) => ((window.innerWidth > 544) ? (window.innerWidth <= 992 ? tablet : desktop) : mobile)
