f5ea0805292f6fca3256641e3c1e7a04851a5669
camila_fiaminghi <camila.fiaminghi@gmail.com>
2019-07-03 11:24:45 -0300

    generate a static version

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
-# SPA Movies
-* install all project dependencies with `npm install` or `yarn install`
-* start the development server with `npm start` or `yarn start`
+## SPA Movies
 
-## This application shows a page with a main banner and lists, each component can display a list of movie related items, as actors , categories, rates or comments. The main components are:
+* Install all project dependencies with `npm install` or `yarn install`
+* Start the development server with `npm start` or `yarn start`
+
+* There is a static version of the application in the /build directory. 
+
+This application shows a page with a main banner and lists, each component can display a list of movie related items, as actors , categories, rates or comments. The main components are:
 
   - Banner
   - Carousel
---
+++ build/precache-manifest.f520049f2af3b7274bdc7977815e1174.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13a3cb5e222b6b86bd7eab16b0a3a0cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9792934b11adeeb94ce",
+    "url": "/static/css/2.c9c511c0.chunk.css"
+  },
+  {
+    "revision": "2690bb6682955f6d95cc",
+    "url": "/static/css/main.66b8ff6e.chunk.css"
+  },
+  {
+    "revision": "a9792934b11adeeb94ce",
+    "url": "/static/js/2.52289bae.chunk.js"
+  },
+  {
+    "revision": "2690bb6682955f6d95cc",
+    "url": "/static/js/main.f92d63ab.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "661c8dd95d523b577ec205e0de477e58",
+    "url": "/static/media/01.661c8dd9.png"
+  },
+  {
+    "revision": "edf6ca4d7e24929afca022a4bd39ecf1",
+    "url": "/static/media/01.edf6ca4d.png"
+  },
+  {
+    "revision": "f49608cfa35f34236c01abdf86187aaa",
+    "url": "/static/media/01.f49608cf.png"
+  },
+  {
+    "revision": "1fbca14beb5987e23f7d97049a43b516",
+    "url": "/static/media/02.1fbca14b.png"
+  },
+  {
+    "revision": "b5e82b40e48cb52f17a97021b78fdba6",
+    "url": "/static/media/02.b5e82b40.png"
+  },
+  {
+    "revision": "e2cde9e08c3355ba7048ea55f05ac8d2",
+    "url": "/static/media/02.e2cde9e0.png"
+  },
+  {
+    "revision": "67a2034690b5a531a98c3e5e361b47d6",
+    "url": "/static/media/03.67a20346.png"
+  },
+  {
+    "revision": "76c555ce289a1eb1bcd842777d97cf8b",
+    "url": "/static/media/03.76c555ce.png"
+  },
+  {
+    "revision": "63f690d40cbae3b4a2279281c1353ec4",
+    "url": "/static/media/04.63f690d4.png"
+  },
+  {
+    "revision": "6e98c95be15ec0e8ebcc57e1773ab82f",
+    "url": "/static/media/04.6e98c95b.png"
+  },
+  {
+    "revision": "145bca7bcbebba8c5120e654d70f1e81",
+    "url": "/static/media/05.145bca7b.png"
+  },
+  {
+    "revision": "f296fca76a86565c382a6142efa910ee",
+    "url": "/static/media/05.f296fca7.png"
+  },
+  {
+    "revision": "39929ee558a1237a2017b3fb43f8466b",
+    "url": "/static/media/06.39929ee5.png"
+  },
+  {
+    "revision": "a78867c51a5787eabdf5f3d0b600fa4e",
+    "url": "/static/media/06.a78867c5.png"
+  },
+  {
+    "revision": "58effa0248ca348e49658417dd34b931",
+    "url": "/static/media/07.58effa02.png"
+  },
+  {
+    "revision": "1dc9fc299fe13222b1d442c289628aa8",
+    "url": "/static/media/08.1dc9fc29.png"
+  },
+  {
+    "revision": "6d2c609edffc4300001271a7e195da59",
+    "url": "/static/media/09.6d2c609e.png"
+  },
+  {
+    "revision": "736a6f66c1b12f33a3d8ac2b6c9539f2",
+    "url": "/static/media/10.736a6f66.png"
+  },
+  {
+    "revision": "29e966c96ea50e104e915f86c315b3ec",
+    "url": "/static/media/App.js.29e966c9.snap"
+  },
+  {
+    "revision": "6e1f0b0fcc77be2f4954b12a93b9c60a",
+    "url": "/static/media/Banner.js.6e1f0b0f.snap"
+  },
+  {
+    "revision": "b113a2bd5ee2ebfbba84b95f165ae120",
+    "url": "/static/media/Carousel.js.b113a2bd.snap"
+  },
+  {
+    "revision": "7acab17865727c1c87f7ba1e26e27632",
+    "url": "/static/media/CarouselItems.js.7acab178.snap"
+  },
+  {
+    "revision": "415337d964f3c5f22ae4f4b3fc4dec84",
+    "url": "/static/media/ExtraComment.js.415337d9.snap"
+  },
+  {
+    "revision": "5b83a539c4255da5047cd528255d48bd",
+    "url": "/static/media/ExtraRate.js.5b83a539.snap"
+  },
+  {
+    "revision": "ea0297736321454865a355b1c023e6a6",
+    "url": "/static/media/ExtrasList.js.ea029773.snap"
+  },
+  {
+    "revision": "e85447fd2c9fc658f1de127c4ac9a657",
+    "url": "/static/media/InfoIcon.js.e85447fd.snap"
+  },
+  {
+    "revision": "571577afd3e25a19251376a3c6eb8845",
+    "url": "/static/media/InfoText.js.571577af.snap"
+  },
+  {
+    "revision": "d593a7144c6572222e8d065d4a724955",
+    "url": "/static/media/InfosList.js.d593a714.snap"
+  },
+  {
+    "revision": "77abda4622ef6383f180e3ce3b65aab8",
+    "url": "/static/media/NavigationControls.js.77abda46.snap"
+  },
+  {
+    "revision": "672eb6470cbefa1f87ec99e639fbd73a",
+    "url": "/static/media/RelatedItems.js.672eb647.snap"
+  },
+  {
+    "revision": "7f44220e409cf9044a4446dcd278415f",
+    "url": "/static/media/TextTruncate.js.7f44220e.snap"
+  },
+  {
+    "revision": "bea4393db81e05910eb4894afc2aeeb9",
+    "url": "/static/media/badmoms.bea4393d.png"
+  }
+]);
